53f54bc89f328111b663bb9d9bf258309e1afc0b
Bogdan-create <bogdan-shigaev@mail.ru>
2021-04-07 23:53:49 +0600

    add front

---
+++ frontEnd/src/components/MoviesForm/MoviesFormHoc.js
@@ -0,0 +1,39 @@
+import { withStyles } from '@material-ui/core/styles'
+import { compose } from 'recompose'
+import { graphql } from 'react-apollo'
+
+import { styles } from './styles'
+
+import { moviesQuery } from '../MoviesTable/queries'
+
+import { addMovieMutation, updateMovieMutation } from './mutations'
+
+import { directorsQuery } from './queries'
+
+const withGraphQL = compose(
+  graphql(addMovieMutation, {
+    props: ({ mutate }) => ({
+      addMovie: (movie) =>
+        mutate({
+          variables: movie,
+          refetchQueries: [{ query: moviesQuery, variables: { name: '' } }],
+        }),
+    }),
+  }),
+  graphql(updateMovieMutation, {
+    props: ({ mutate }) => ({
+      updateMovie: (movie) =>
+        mutate({
+          variables: movie,
+          refetchQueries: [{ query: moviesQuery, variables: { name: '' } }],
+        }),
+    }),
+  }),
+  graphql(directorsQuery, {
+    options: ({ name = '' }) => ({
+      variables: { name },
+    }),
+  })
+)
+
+export default compose(withStyles(styles), withGraphQL)
